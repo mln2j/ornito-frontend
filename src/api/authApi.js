bfb4d987bcdc11d1edf7d677704d7ae0c5bb7b8b
Mihael Lendvaj <lendvaj.miha@gmail.com>
2025-02-11 20:55:16 +0100

    Update authApi.js

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5156"; // prilagodi ako backend koristi drugačiji port
+const API_URL = "http://localhost:5156/api"; // prilagodi prema C# backendu
 
 export const register = async (userData) => {
-    const response = await axios.post(`${API_URL}/register`, userData);
+    const response = await axios.post(`${API_URL}/auth/register`, userData);
     return response.data;
 };
 
 export const login = async (credentials) => {
-    const response = await axios.post(`${API_URL}/login`, credentials);
+    const response = await axios.post(`${API_URL}/auth/login`, credentials);
     return response.data;
 };
