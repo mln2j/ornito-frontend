f2e192f2588e0b519f84f88ceca8261ac2f74d8f
Mihael Lendvaj <lendvaj.miha@gmail.com>
2025-02-23 22:45:25 +0100

    Add HomePage styling

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,41 @@
 import React from "react";
+import { Link } from "react-router-dom"; // Koristimo Link iz React Router-a za navigaciju
+import { jwtDecode } from "jwt-decode";
+import Cookies from "js-cookie";
 
 const HomePage = () => {
+    const token = Cookies.get("auth_token");
+    let username = "";
+
+    if (token) {
+        try {
+            const decoded = jwtDecode(token);
+            username = decoded.name;
+        } catch (error) {
+            console.error("Greška pri dekodiranju tokena:", error.message);
+        }
+    } else {
+        console.error("Token nije pronađen!");
+    }
+
     return (
-        <div>
-            <h2>Dobrodošao! Uspješno prijavljen.</h2>
-            <p>Ovo je početna stranica aplikacije.</p>
+        <div className="container">
+            <h2>Pozdrav {username}!</h2>
+            <div className="controls">
+                <Link to="/scan" className="scanLink">
+                    <div>
+                        <p>Novi</p>
+                        <p>Sken</p>
+                    </div>
+                </Link>
+                {/* Link za profil */}
+                <Link to="/profile" className="profileLink">
+                    <div>
+                        <p>Moj</p>
+                        <p>Profil</p>
+                    </div>
+                </Link>
+            </div>
         </div>
     );
 };
