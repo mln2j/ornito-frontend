dd71337615099047f423d7aa7bc94e6a5ed6ff1b
Mihael Lendvaj <lendvaj.miha@gmail.com>
2025-02-23 18:35:09 +0100

    Add container to nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,15 +4,17 @@ import {NavLink} from "react-router-dom";
 const Navbar = ({isAuthenticated, logout}) => {
     return (
         <nav>
-            <NavLink
-                to="/"
-            >
-                Ornito
-            </NavLink>
+            <div className="container navContainer">
+                <NavLink
+                    to="/"
+                >
+                    Ornito
+                </NavLink>
 
-            {isAuthenticated && (
-                <a href="/login" onClick={logout}>Odjava</a>
-            )}
+                {isAuthenticated && (
+                    <a href="/login" onClick={logout}>Odjava</a>
+                )}
+            </div>
         </nav>
     );
 };
